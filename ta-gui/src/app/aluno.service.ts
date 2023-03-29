1d61dc29356da70dd42e3f142d06aa9692a9cce9
jorge-junior <91333173+jorge-junior@users.noreply.github.com>
2023-03-29 09:28:48 -0300

    cadastro de metas, e clonagem de objetos para simular funcionamento do servidor

--- ta-gui/src/app/aluno.service.ts
+++ ta-gui/src/app/aluno.service.ts
@@ -6,7 +6,8 @@ import { Aluno } from './aluno';
 export class AlunoService {
   alunos: Aluno[] = [];
   
-   gravar(aluno: Aluno): Aluno | null {
+   criar(aluno: Aluno): Aluno | null {
+    aluno = aluno.clone();
     var result = null;
     if (this.cpfNaoCadastrado(aluno.cpf)) {
       this.alunos.push(aluno);
@@ -17,4 +18,14 @@ export class AlunoService {
   cpfNaoCadastrado(cpf: string): boolean {
      return !this.alunos.find(a => a.cpf == cpf);
   }
+  
+  atualizar(aluno:Aluno): void {
+    aluno = aluno.clone();
+    for (let a of this.alunos) {
+      if (a.cpf == aluno.cpf) {
+        a.metas = aluno.metas;
+      }
+    }
+  }
+  
 }
