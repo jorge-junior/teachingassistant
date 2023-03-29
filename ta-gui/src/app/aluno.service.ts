80c24e06eb4bf8882d4e596bf0bb581e2130111a
jorge-junior <91333173+jorge-junior@users.noreply.github.com>
2023-03-29 08:38:35 -0300

    Update aluno.service.ts

--- ta-gui/src/app/aluno.service.ts
+++ ta-gui/src/app/aluno.service.ts
@@ -4,7 +4,7 @@ export class AlunoService {
   alunos: Aluno[] = [];
   
    gravar(aluno: Aluno): Aluno {
-    var result = NULL;
+    var result = null;
     if (!this.alunos.find(a => a.cpf == aluno.cpf)) {
       this.alunos.push(aluno);
       result = aluno;
